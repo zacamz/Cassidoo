3982b819ba1a8189b249ac5b355fd1b6c60d7847
Zachary Johnson <vepped@gmail.com>
2025-12-09 20:24:03 -0500

    Getting closer
    Created the deck and debugged that
    Starting to work on the shuffle function but a racking my brain to try and figure out where to start

--- ShuffleDeck.py
+++ ShuffleDeck.py
@@ -1,15 +1,36 @@
+import random
+
 suits = ["♠","♥","♦","♣"]
 
 royalty = ["J","Q","K","A"]
 
 def createDecks(number_of_decks:int):
     decks= []
-    for deck in range(0,number_of_decks):
+    for loop in range(0,number_of_decks):
         cards = []
         for suit in enumerate(suits):
-            for num in range(0,9):
-                cards+= f"{num}{suit}"
-        decks+= cards
+            for num in range(2,11):
+                card = str(num)+suit[1]
+                cards.append(card)
+            for special in enumerate(royalty):
+                card = special[1]+suit[1]
+                cards.append(card)
+        decks += cards
     return decks
 
-print(createDecks(1))
+deck = createDecks(1)
+print(deck)
+def shuffle(deck):
+        copydeck = deck
+        print(len(copydeck))
+        half= int(len(copydeck)/2)
+        print(half)
+        adeck=copydeck[:half]
+        bdeck=copydeck[half:]
+        for card in range(0,int(len(copydeck))):
+                print(random.randint(0,1))
+        
+        return copydeck
+
+deck = shuffle(deck)
+print(deck)
